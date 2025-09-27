963a05cc3673e909fefc9b409fb6276edd44fab2
Isabella <isabellafj97@gmail.com>
2025-09-26 22:23:55 -0500

    fix vue config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			vue: '@vue/compat',
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		},
-		dedupe: ['vue']
-	},
-	plugins: [vue(), vueJsx()]
-});
+  base: '/', 
+  resolve: {
+    alias: {
+      vue: '@vue/compat',
+	  // @ts-ignore
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    dedupe: ['vue'],
+  },
+  plugins: [vue(), vueJsx()],
+  build: {
+    outDir: 'dist',
+  },
+})
